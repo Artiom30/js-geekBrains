0eee7d7ef3e4024f469d0c61dca1c1ebeb98eb82
Artiom30 <mezheynikov1@gmail.com>
2023-03-17 22:25:30 +0300

    seminar1

---
+++ seminars/seminar1/task6/script.js
@@ -0,0 +1,25 @@
+// Задание 6
+
+// 1.Создайте строку с вашем именем и строку с вашей фамилией.Выведите эти данные в консоль
+
+// 2.Создайте переменную с текстом 'java' и переменную с текстом 'script'.С помощью этих переменных и операции сложения строк выведите в консоль строку 'javascript'.
+
+// 3.Создайте переменную с текстом 'hello' и переменную с текстом 'world'.С помощью этих переменных и операции сложения строк выведите в консоль строку 'hello world'.
+
+const name = 'Artiom';
+const lastName = 'Mezheynikov';
+
+console.log(name, lastName);
+
+const java = 'Java';
+const script = 'Script';
+
+// console.log(`${java}${script}`);
+
+const hello = 'Hello';
+const world = 'World';
+// Конкотинация строки в JS. Слождение строк.
+console.log(hello + ' ,' + world + '!');
+
+
+
