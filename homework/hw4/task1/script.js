ce02213253befa982f7069136281f71ab03e6492
Artiom30 <mezheynikov1@gmail.com>
2023-03-30 05:37:37 +0300

    Hw4Task1

--- homework/hw4/task1/script.js
+++ homework/hw4/task1/script.js
@@ -8,4 +8,14 @@
 …
 10 – четное число
  */
-
+function showElevenNumbers() {
+	// console.log('0 - is zero');
+	for (let i = 0; i <= 11; i++) {
+		(i > 0)
+			? (i % 2 == 0)
+				? console.log(`${i} - is an odd number`)
+				: console.log(`${i} - is an even number`)
+			: console.log('0 - is zero');
+	}
+}
+showElevenNumbers();
