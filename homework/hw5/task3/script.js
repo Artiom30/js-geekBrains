a7314c48768ae71ef4adcd3abfab8b8868d5bcc2
Artiom30 <mezheynikov1@gmail.com>
2023-04-02 00:57:52 +0300

    HW5Task3

--- homework/hw5/task3/script.js
+++ homework/hw5/task3/script.js
@@ -17,4 +17,22 @@
 		},
 		];
  */
+const products = [
+	{
+		id: 3,
+		price: 200,
+	},
+	{
+		id: 4,
+		price: 900,
+	},
+	{
+		id: 1,
+		price: 1000,
+	},
+];
 
+products.forEach(el => {
+	el.price = el.price * 0.85;
+});
+console.log(products);
