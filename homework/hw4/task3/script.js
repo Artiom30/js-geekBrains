6fcdf03b108e81280de1f5d494c0534ad1a1ddb9
Artiom30 <mezheynikov1@gmail.com>
2023-04-02 00:28:20 +0300

    HW4Task3

--- homework/hw4/task3/script.js
+++ homework/hw4/task3/script.js
@@ -2,4 +2,11 @@
 /* 
 Используя Math.random() вам необходимо генерировать цифры от 0 до 9, и создать массив состоящий из 5 таких элементов
  */
-
+const newArray = [];
+for (let i = 0; i < 5; i++) {
+	newArray[i] = Math.round(Math.random() * 10);
+	if (newArray[i] == 10) {
+		newArray[i] -= 1;
+	}
+}
+console.log(newArray);
