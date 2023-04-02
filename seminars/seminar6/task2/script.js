0c94ec9d66379129d0404f4c60823d5768f070ff
Artiom30 <mezheynikov1@gmail.com>
2023-04-02 21:37:01 +0300

    Seminar6Initial

---
+++ seminars/seminar6/task2/script.js
@@ -0,0 +1,8 @@
+//  Задание 2
+/*
+1. Дан тег <a class="link" href="#">link text html</a>
+	 Вам необходимо поменять текст внутри ссылки на “link text js”
+	 Заменить href, на значение https://developer.mozilla.org/ru/
+2. Дан тег <img class="photo" src="" alt="">
+	 Вам необходимо с помощью js поменять значение src на любое изображение из интернета
+*/
