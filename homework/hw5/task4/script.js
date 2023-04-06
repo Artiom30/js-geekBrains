51c781243883df45d1716f8cf6bb2f53fd494db8
Artiom30 <mezheynikov1@gmail.com>
2023-04-06 21:35:25 +0300

    hw5Task4

--- homework/hw5/task4/script.js
+++ homework/hw5/task4/script.js
@@ -3,35 +3,32 @@
 1.  Необходимо вывести в консоль массив продуктов в котором есть хоть одна фотография используя метод filter. Исходные данные - массив products.
 2.  Необходимо отсортировать массив products используя метод sort по цене, начиная с самой маленькой, заканчивая самой большой ценой, после чего вывести отсортированный массив в консоль.
 
-				const products = [
-				{
-				id: 3,
-				price: 127,
-				photos: [
-				"1.jpg",
-				"2.jpg",
-				],
-				},
-				{
-				id: 5,
-				price: 499,
-				photos: [],
-				},
-				{
-				id: 10,
-				price: 26,
-				photos: [
-				"3.jpg",
-				],
-				},
-				{
-				id: 8,
-				price: 78,
-				},
-				];
-
-				[]()
-				[]()
+const products = [
+	{
+		id: 3,
+		price: 127,
+		photos: [
+			"1.jpg",
+			"2.jpg",
+		],
+	},
+	{
+		id: 5,
+		price: 499,
+		photos: [],
+	},
+	{
+		id: 10,
+		price: 26,
+		photos: [
+			"3.jpg",
+		],
+	},
+	{
+		id: 8,
+		price: 78,
+	},
+];
  */
 
 const products = [
@@ -62,17 +59,16 @@ const products = [
 ];
 
 // 1
-/*
+
 console.log(products);
 // products.filter(el => el.photos !== undefined && el.photos.length !== 0).forEach(el => console.log(el)); Вы наверное делаете из нас колбасников... =)
 const poroductWithImg = products.filter(el => el.photos !== undefined && el.photos.length !== 0)
 console.log(poroductWithImg);
- */
+
 
 // 2
 
-products.forEach((el, i, array) => {
-	array[i].price.sort((a, b) => a - b)
-});
+products.sort((a, b) => a.price - b.price);
 console.log(products);
+
 
--- homework/hw5/task5/script.js
+++ homework/hw5/task5/script.js
@@ -7,4 +7,20 @@
 
 Вам необходимо объединить 2 этих массива, чтобы значения первого массива были ключами, а значения второго массива — значениями.
  */
+const en = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+const ru = ["понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье"];
 
+// const objectKeyWeekEng = Object.assign({}, en);
+// console.log(objectKeyWeekEng);
+
+// const objectWeek = {};
+
+function objetKeys(array) {
+	const object = {};
+	for (let i = 0; i < array.length; i++) {
+		object[array[i]] = array[i];
+	}
+	return object;
+}
+const objectWeek = objetKeys(en);
+console.log(objectWeek);
