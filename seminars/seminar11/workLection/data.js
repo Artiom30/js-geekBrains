67e277002718694f017bf021ae24223f6439f3cb
Artiom30 <mezheynikov1@gmail.com>
2023-04-23 19:33:43 +0300

    seminar11Final

---
+++ seminars/seminar11/workLection/data.js
@@ -0,0 +1,26 @@
+export const dataProducts = `[
+	{
+		"name": "MANGO  PEOPLE  T-SHIRT",
+		"image": "img/product1.jpg",
+		"price": 300,
+		"color": "red",
+		"size": "Xl",
+		"quantity": 2
+	},
+	{
+		"name": "ELLERY X M'O CAPSULE",
+		"image": "img/product2.jpg",
+		"price": 52,
+		"color": "black",
+		"size": "Xl",
+		"quantity": 1
+	},
+	{
+		"name": "Shtani +++",
+		"image": "img/product1.jpg",
+		"price": 1000,
+		"color": "green",
+		"size": "XXXl",
+		"quantity": 10
+	}
+]`;
